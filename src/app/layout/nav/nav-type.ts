39c366c9aa91cfd189e4c28c062d9155f448cf11
Tony <tonyle.dev@gmail.com>
2020-10-04 00:09:34 +0700

    feat: build layout

---
+++ src/app/layout/nav/nav-type.ts
@@ -0,0 +1,25 @@
+export interface NavItem {
+  id: string;
+  title: string;
+  type: 'item' | 'group' | 'collapsable';
+  translate?: string;
+  icon?: string;
+  hidden?: boolean;
+  url?: string;
+  classes?: string;
+  exactMatch?: boolean;
+  externalUrl?: boolean;
+  openInNewTab?: boolean;
+  function?: any;
+  badge?: {
+    title?: string;
+    translate?: string;
+    bg?: string;
+    fg?: string;
+  };
+  children?: NavItem[];
+}
+
+export interface NavType extends NavItem {
+  children?: NavItem[];
+}
---
+++ src/app/layout/nav/nav.ts
@@ -0,0 +1,5 @@
+import { NavType } from './nav-type';
+
+export const nav: NavType[] = [
+
+];
